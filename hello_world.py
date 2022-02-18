a6fbc0e0103af208c215082ec9fe5c5b5796d79e
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-02-18 23:29:51 +0000

    added hello world3

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print("Hello, world")
+#!/usr/bin/env python3
+
+# Created by: Huzaifa Khalid
+# Created on: Febuary 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+    
+if __name__ == "__main__":
+    main()
+    
